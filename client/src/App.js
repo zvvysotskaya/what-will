d2d50eea376c1e4cb94d06885c3e3a231bf69dc8
zvvysotskaya <zhannavyvy@gmail.com>
2020-05-14 21:32:05 -0400

    adding error-boundary component, suspense component , and react.lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Suspense }  from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/home-page/home-page.page';
-import ShoppingPage from './pages/shopping-page/shopping-page.page';
+import ErrorBoundary from './components/error-boundary/error-boundary';
 import Header from './partial/header/header.partial';
-import SignUp from './pages/signup-page/signup-page.page';
-import CreateShoppingListPage from './pages/create-shopping-list-page/create-shopping-list-page.page';
-import LoginPage from './pages/login-page/login-page.page';
+const HomePage = React.lazy(() => import('./pages/home-page/home-page.page'));
+const ShoppingPage = React.lazy(() => import('./pages/shopping-page/shopping-page.page'));
+const SignUp = React.lazy(()=> import('./pages/signup-page/signup-page.page'));
+const CreateShoppingListPage = React.lazy(() => import('./pages/create-shopping-list-page/create-shopping-list-page.page'));
+const LoginPage = React.lazy(() => import('./pages/login-page/login-page.page'));
     
 function App() {
   return (
       <div>
           <Header/>
           <Switch>
-              <Route exact path='/' component={HomePage} />
-              <Route exact path='/shoppingPage' component={ShoppingPage} />
-              <Route exact path='/signupPage' component={SignUp} />
-              <Route exact path='/createShoppingListPage' component={CreateShoppingListPage} />
-              <Route exact path='/loginPage' component={LoginPage} />
+              <ErrorBoundary>
+                  <Suspense fallback={<div className="spinner-border text-success justify-content-center" style={{
+                      width: 5 + 'rem', height: 5 + 'rem',
+                      position: 'absolute', display: 'block', top: 40 + '%', left: 50 + '%'
+                  }}>
+                  </div>}>
+                      <Route exact path='/' component={HomePage} />
+                      <Route exact path='/shoppingPage' component={ShoppingPage} />
+                      <Route exact path='/signupPage' component={SignUp} />
+                      <Route exact path='/createShoppingListPage' component={CreateShoppingListPage} />
+                      <Route exact path='/loginPage' component={LoginPage} />
+                  </Suspense>
+              </ErrorBoundary>
           </Switch>
     </div>
   );
